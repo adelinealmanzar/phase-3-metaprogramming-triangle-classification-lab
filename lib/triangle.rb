4b0338507fc0045d67142a60aa52f79fa7cb9585
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-02-25 12:15:20 -0800

    commit tree e72a38b859575bfca13dbdf6129de513d704d2dd

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :l1, :l3, :l3, :equilateral
+
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def validate_triangle?
+    valid_size = @l1 > 0 && @l2 > 0 && @l3 > 0
+    triangle_inequality = @l1 + @l2 > @l3 && @l2 + @l3 > @l1 && @l3 + @l1 > @l2
+    valid_size && triangle_inequality
+  end
+
+
+  def test_triangle
+    raise TriangleError unless validate_triangle?
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def kind
+    self.test_triangle;
+    if @l1 == @l2 && @l2 == @l3
+      :equilateral
+    elsif @l1 == @l2 || @l2 == @l3 || @l1 == @l3
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
+
+# Triangle.new(0, 0, 0).kind
